2e5f9485fc9b51dec59c0ab367d3c890bd41cea9
Martins Luters <luters.martins@gmail.com>
2022-11-24 14:05:48 +0200

    Add Snyk CLI global npm package

--- npm_global_packages.sh
+++ npm_global_packages.sh
@@ -4,3 +4,6 @@ npm install -g wp-local-docker
 # Selenium standalone
 npm install -g selenium-standalone
 selenium-standalone install
+
+# Snyk CLI > Test vulnerable npm modules in node project
+npm install snyk -g
